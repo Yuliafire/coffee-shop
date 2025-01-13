bfad9b177fd7cbcf6c78273df105aede753f55ec
Yulia <linguavoice1@yahoo.com>
2025-01-13 23:00:47 +0300

    refactor: refactor fav coffee section including media styles - moving to styles.css from media.css file

--- coffee-shop/src/common/index.js
+++ coffee-shop/src/common/index.js
@@ -1,30 +1,27 @@
 // BURGER
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".header").classList.remove("open");
+  document.querySelector(".header").classList.remove("open");
 
-    document.getElementById("burger").addEventListener('click', () => {
-        window.scrollTo({
-            top:0,
-        })
-        document.querySelector(".header").classList.toggle("open");
-        document.body.classList.toggle("menu-open");
-    })
-
-    document.querySelectorAll('.menu__link').forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const targetUrl = link.getAttribute('href');
-
-            document.querySelector('.header').classList.remove('open');
-            document.body.classList.remove('menu-open');
-
-            setTimeout(() => {
-                window.location.href = targetUrl;
-            }, 300);
-        });
-      })
+  document.getElementById("burger").addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
     });
+    document.querySelector(".header").classList.toggle("open");
+    document.body.classList.toggle("menu-open");
+  });
 
+  document.querySelectorAll(".menu__link").forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetUrl = link.getAttribute("href");
 
+      document.querySelector(".header").classList.remove("open");
+      document.body.classList.remove("menu-open");
 
+      setTimeout(() => {
+        window.location.href = targetUrl;
+      }, 300);
+    });
+  });
+});
